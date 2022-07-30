e86fb621c462bb57a61e77047f182ecd1b2827cd
Sorokin2204 <daniil.sorokin.228@gmail.com>
2022-07-30 19:17:18 +0300

    Prop

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 });
 app.use(handleError);
 
-const PORT = 80;
+const PORT = process.env.PORT || 30;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
