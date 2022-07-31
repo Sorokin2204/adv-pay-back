b6fc7a26a878fd6be77bcb6318a2c99725d51a06
Sorokin2204 <daniil.sorokin.228@gmail.com>
2022-07-31 15:54:56 +0300

    wm

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.use(function (req, res, next) {
 });
 app.use(handleError);
 
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+// const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
