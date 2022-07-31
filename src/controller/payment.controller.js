55ebe20ef6a673858fcd67c3fda4fe9c075c8c91
Sorokin2204 <daniil.sorokin.228@gmail.com>
2022-07-31 16:11:51 +0300

    wm

--- src/controller/payment.controller.js
+++ src/controller/payment.controller.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const { CustomError, TypeError } = require('../models/customError.model');
 const Package = db.packages;
+const User = db.users;
 const axios = require('axios');
 const cheerio = require('cheerio');
 class PackageController {
@@ -25,15 +26,34 @@ class PackageController {
     res.send('YES');
   }
   async paymentProcess(req, res) {
-    const { LMI_PAYEE_PURSE, LMI_PAYMENT_AMOUNT, LMI_PAYMENT_NO, LMI_MODE, LMI_SYS_INVS_NO, LMI_SYS_TRANS_NO, LMI_SYS_TRANS_DATE, LMI_PAYER_PURSE, LMI_PAYER_WM, LMI_HASH } = req.body;
+    const { LMI_PAYEE_PURSE, LMI_PAYMENT_AMOUNT, LMI_PAYMENT_NO, LMI_MODE, LMI_SYS_INVS_NO, LMI_SYS_TRANS_NO, LMI_SYS_TRANS_DATE, LMI_PAYER_PURSE, LMI_PAYER_WM, LMI_HASH, token } = req.body;
     if (LMI_PAYEE_PURSE === 'Z157035074475') {
       const hashStr = ''.concat(LMI_PAYEE_PURSE, LMI_PAYMENT_AMOUNT, LMI_PAYMENT_NO, LMI_MODE, LMI_SYS_INVS_NO, LMI_SYS_TRANS_NO, LMI_SYS_TRANS_DATE, 'test123', LMI_PAYER_PURSE, LMI_PAYER_WM);
       console.log(LMI_HASH);
-      const hashGen = crypto.createHash('sha256').update(hashStr).digest('hex');
-      console.log(hashGen);
-      console.log(hashGen === LMI_HASH);
+      const hashGen = crypto.createHash('sha256').update(hashStr).digest('hex').toUpperCase();
+
+      if (hashGen == LMI_HASH) {
+        const tokenData = jwt.verify(token, 'secret-jwt-pass', (err, tokenData) => {
+          if (err) {
+            res.json('NO');
+          }
+          return tokenData;
+        });
+
+        const findUser = await User.findOne({
+          where: {
+            id: tokenData?.id,
+            email: tokenData?.email,
+          },
+        });
+        if (findUser) {
+            console.log(findUser);
+        res.json('YES');
+      } else {
+        res.json('NO');
+      }
     }
-    res.send('YES');
+    res.json('NO');
   }
 }
 
